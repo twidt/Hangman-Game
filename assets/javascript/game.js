209ef9bafe3de059ccfe6da55d34363854c15bed
Twidt <t.c.twidt@gmail.com>
2018-02-07 01:16:44 -0500

    deletion of extra index file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,38 @@
 $(document).ready(function(){
+    console.log ("testing3")
 // create words and random selector
 // number of underscores correlates to the randomly selected word in the array
-var secretWords = ['Jake', 'Finn', 'Bubblegum', 'Marciline', 'Rainicorn', 'Peppermint', 'Tart'],
-    random = Math.floor(Math.random() * secretWords.length),
-    randomWord = secretWords[random];
+var secretWords = ['Jake', 'Finn', 'Bubblegum', 'Marciline', 'Rainicorn', 'Peppermint', 'Tart'];
+var random = Math.floor(Math.random() * secretWords.length);
+var randomWord = secretWords[random];
 var result = "";
 var answer = [];
-function blanksFromWord(word) {
+var gameOn = false;
 
-  
-  for (var i = 0; i < secretWords.length; i++) {
-    result += "_";
-  }
-  return result;
+if(gameOn = false){
+    $(document).keyup(function(){
+        gameOn = true
+        console.log("testing2")
+    });
 }
-$(".card-text").html(result)
+
+if(gameOn=true){
+    function blanksFromWord(word) {
+        for (var i = 0; i < secretWords.length; i++) {
+        result += "_";
+        console.log("testing")
+
+    }
+    $(".card-text").html(result)
 //console.log(randomWord);//
 //console.log(blanksFromWord(randomWord));//
 // (blanksFromRandom(randomWord))
 //first, I'm going to give the letters that can be picked
-var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphabetLetters = secretWords.length;
+    var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var lengthofsecretWords = secretWords.length;
         
         //The playing loop
-        while (alphabetLetters > 0) {
+        // while (alphabetLetters > 0) {
             //This will be the players progress
             alert("This is the number of remaining letters:\n" + answer.join(" "));
             
@@ -32,23 +41,26 @@ var alphabetLetters = secretWords.length;
             if (prompt === null) {
                 //Game has ended because player left
                 break;
-            } else if (guess.length !== 1) {
+            } else if (secretWords.length !== 1) {
                 alert("Please enter one single letter.");
             } else {
                 //Update match with guess
-                for (var j = 0; j < word.length; j++) {
-                    if (word[j] === guess) {
+                for (var j = 0; j < secretWords.length; j++) {
+                    if (secretWords[j] === guess) {
                         answer[j] = guess;
                         remainingLetters--;
-                    }
-                }
+                //     }
+                // }
             }
             //End of playing loop
         }
         
         //Show answer and congratulate the player
         alert(answer.join(" "));
-alert("Good work! The right answer is " + word);
+        alert("Good work! The right answer is " + word);
+        }
+    }
+}
 });
 //now I need to figure out how to get the above to run inside the card
 
